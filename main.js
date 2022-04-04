d3d2411784a1bd0883f5d22d48d59504c7cd1608
VasoC <vasochkhartishvili@gmail.com>
2022-04-04 16:37:29 +0400

    add js Primitive data

---
+++ main.js
@@ -0,0 +1,22 @@
+let userName = "Vaso";
+let userName2 = "Vasil";
+// console.log(userName, userName2);
+let userAge = "26";
+let userAge2 = "20";
+// console.log(userName, userName2, userAge2, userAge2);
+const userId = "vaso.ch";
+// console.log(userId);
+let isEmployed = true;
+// console.log(isEmployed);
+let myNan = "Nan";
+let myNull = "null";
+// console.log(myNan, myNull);
+
+// My information
+let myName = "Vasil";
+let myHobby = "Surfing";
+let yearOfBirth = "1996";
+let currentYear = "2022";
+// console.log(myName, myHobby, yearOfBirth, currentYear);
+let myAge = currentYear - yearOfBirth;
+// console.log(myAge);
