c91da2d4bef073e851189a0e2d49d4a3eba14e0f
VasoC <vasochkhartishvili@gmail.com>
2022-04-04 17:24:17 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let userName = "Vaso";
 let userName2 = "Vasil";
 // console.log(userName, userName2);
-let userAge = "26";
-let userAge2 = "20";
+let userAge = 26;
+let userAge2 = 20;
 // console.log(userName, userName2, userAge2, userAge2);
 const userId = "vaso.ch";
 // console.log(userId);
@@ -14,9 +14,15 @@ let myNull = "null";
 
 // My information
 let myName = "Vasil";
-let myHobby = "Surfing";
-let yearOfBirth = "1996";
-let currentYear = "2022";
+let myHobby = "Basketball";
+let yearOfBirth = 1996;
+let currentYear = 2022;
 // console.log(myName, myHobby, yearOfBirth, currentYear);
 let myAge = currentYear - yearOfBirth;
 // console.log(myAge);
+// console.log(typeof yearOfBirth);
+
+// sentence
+let	mySentence = `My name is ${myName}, I am ${myAge} years old and my hobby is playing a ${myHobby}`
+console.log(mySentence);
+console.log("My name is" + " " + myName + ", I am" + " " + myAge + " " + "years old and my hobby is playing a" + " " + myHobby);
